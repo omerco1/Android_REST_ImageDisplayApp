34b2e3b7d07220b4a09df8d9112aeb9e0fad81ff
omerco1 <31459523+omerco1@users.noreply.github.com>
2018-11-10 21:36:31 -0800

    implemented camera fab functionality, updated manifest

--- java/edu/ucsb/cs/cs184/uname/imageratingexplorer/MainActivity.java
+++ java/edu/ucsb/cs/cs184/uname/imageratingexplorer/MainActivity.java
@@ -1,11 +1,14 @@
 package edu.ucsb.cs.cs184.uname.imageratingexplorer;
 
+import android.Manifest;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
@@ -14,8 +17,14 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.BaseColumns;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -45,7 +54,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import org.json.*;
 
@@ -58,6 +70,12 @@ public class MainActivity extends AppCompatActivity {
     jsonResponseAdapter jsonAdapt;
     MyAdapter gvAdapter;
     GridView gv;
+    View v;
+    String photoFileName;
+    public final String APP_TAG = "MyCustomApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+
+
 
     ImageRatingDbHelper dbObj;
 
@@ -99,7 +117,6 @@ public class MainActivity extends AppCompatActivity {
         RatingBar rb = (RatingBar) findViewById(R.id.ratingBar);
 
 
-
         ImageRatingDbHelper.initialize(this);
 
         final Bitmap myBmap;
@@ -139,6 +156,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        if	(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                ==	PackageManager.PERMISSION_GRANTED)	{}
+        else	{
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]
+                    {Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }
+
         cam = (FloatingActionButton) findViewById(R.id.cameraFAB);
         cam.setImageResource(android.R.drawable.ic_menu_camera);
         gal = (FloatingActionButton) findViewById(R.id.galleryFAB);
@@ -149,6 +174,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //Toast.makeText(getApplicationContext(), "Camera functionality has been left as an excercise for the grader", Toast.LENGTH_LONG);
                 Log.d("Camera FAB" , "Im in cam onCLick listener");
+                cameraHandler();
+
             }
         });
 
@@ -158,6 +185,7 @@ public class MainActivity extends AppCompatActivity {
 //                Toast.makeText(getApplicationContext(), "gallary functionality has been left as an excercise for the grader", Toast.LENGTH_LONG);
 //                Toast.makeText(myOnCreateContext, "hi", Toast.LENGTH_SHORT);
                 Log.d("Galler FAB" , "Im in gal onCLick listener");
+                galleryHandler();
 
             }
         });
@@ -168,6 +196,96 @@ public class MainActivity extends AppCompatActivity {
         bmArrList = bmA;
 
 
+    }
+
+
+
+    public void  cameraHandler() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Date currentTime = Calendar.getInstance().getTime();
+        Log.d("current time", currentTime.toString());
+        photoFileName = "camera_" + currentTime.toString().replace(":", "");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName));
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri takenPhotoUri = getPhotoFileUri(photoFileName);
+                // by this point we have the camera photo on disk
+                ImageRatingDbHelper.getInstance().addPost( photoFileName, takenPhotoUri.toString(), 0);
+                gvAdapter.resetArrayLists(0);
+                gvAdapter.notifyDataSetInvalidated();
+
+                int id = gvAdapter.myIdArr.get(gvAdapter.myIdArr.size()-1);
+
+//                myDiaFrag df = myDiaFrag.newInstance(id, takenPhotoUri.toString(),  0);
+//                df.show(getSupportFragmentManager(), "title");
+
+
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == 10 && resultCode == RESULT_OK){
+            Uri uri = intent.getData();
+            String [] projection = {MediaStore.Images.Media.DATA};
+
+            Cursor c = getContentResolver().query(uri,projection,null,null,null);
+            c.moveToFirst();
+            int cI = c.getColumnIndex(projection[0]);
+            String path = c.getString(cI);
+            c.close();
+
+            Date currentTime = Calendar.getInstance().getTime();
+
+             ImageRatingDbHelper.getInstance().addPost(currentTime.toString(), "file://" + path,0);
+             gvAdapter.resetArrayLists(0);
+             gvAdapter.notifyDataSetInvalidated();
+
+        }
+    }
+
+    // Returns the Uri for a photo stored on disk given the fileName
+    public Uri getPhotoFileUri(String fileName) {
+        Log.d("IM HERE", "here");
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+
+            File mediaStorageDir = new File( getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+            Log.d("file path", file.getAbsolutePath());
+            // wrap File object into a content provider, required for API >= 24
+            //TODO CHANGE UNAME
+            Log.d("app id", BuildConfig.APPLICATION_ID);
+            //edu.ucsb.cs.cs184.uname.imageratingexplorer.
+            Uri temp = FileProvider.getUriForFile(getApplicationContext(), BuildConfig.APPLICATION_ID + ".fileprovider", file);
+
+            return temp;
+        }
+        return null;
+    }
+
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        return state.equals(Environment.MEDIA_MOUNTED);
+    }
+
+    public void galleryHandler() {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, 11);
     }
 
     @Override
@@ -246,9 +364,4 @@ public class MainActivity extends AppCompatActivity {
           for (int i = 0; i < myArrList.size(); i++)
             myArrList.add(myArrList.get(i));
     }
-
-
-
-
-
 }
